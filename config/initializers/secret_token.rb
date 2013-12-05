2057aebe5f7da6cd516c2d7d15d0d61c8c69ae92
Kyle Alonius <k_alonius@hotmail.com>
2013-12-04 18:18:14 -0600

    First commit, empty rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UltimateChallenge6Crm::Application.config.secret_token = '9a528641915ec02f5f8d59bcbf132389ca4181602ef43c43b1eb70a8caa185fbd61361ca869fca4c70623bea2296974124a0a2ffebecbba5709717acc5638cf5'
